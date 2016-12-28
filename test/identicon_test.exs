4ae5c43bfb22ca3d79b418183e23b632d8d75b9c
Jovanny Cruz <jovannypcg@yahoo.com>
2016-12-28 12:13:04 -0600

    Add test for the hash function

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,16 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "hash should return an Image with a hex property" do
+    input = "testingHash"
+
+    expected = %Identicon.Image{
+      hex: [171, 77, 107, 248, 117, 196, 29, 219,
+            27, 71, 64, 188, 13, 185, 97, 145]
+    }
+
+    current = Identicon.hash(input)
+
+    assert expected == current
   end
 end
