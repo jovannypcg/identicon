7e22d65b3faafa18d6415d320b14848cbf445815
Jovanny Cruz <jovannypcg@yahoo.com>
2016-12-28 13:11:47 -0600

    Refactor build_grid so that it returns a new Identicon.Image with a grid property

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash
     |> pick_color
+    |> build_grid
   end
 
   def hash(input) do
@@ -16,5 +17,24 @@ defmodule Identicon do
     [r, g, b | _tail] = image.hex
 
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirrow_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirrow_row(row) do
+    # [145, 46, 200]
+    [first, second | _tail] = row
+
+    # [145, 46, 200, 46, 145]
+    row ++ [second, first]
   end
 end
